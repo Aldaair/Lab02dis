078314db8a5a881307a9e7b7ad50a1b250ef5ac3
Aldaaairr <86060406+Aldaaairr@users.noreply.github.com>
2022-03-26 00:53:32 -0500

    Update js.js

--- js.js
+++ js.js
@@ -3,10 +3,10 @@ var nombre = document.getElementById("user").value;
 var email = document.getElementById("email").value;
 
 var button1 = document.getElementById("button1");
-let button2 = document.getElementById("button2");
-let button3 = document.getElementById("button3");
-let button4 = document.getElementById("button4");
-let button5 = document.getElementById("button5");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var button5 = document.getElementById("button5");
 
 button1.onclick=function(){
     var nombre = document.getElementById("user").value;
